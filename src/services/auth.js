bc65d1979b5911eb70d718b89e99971ef1b6148d
denizlg24 <denizlg24@gmail.com>
2023-02-22 00:22:23 +0000

    Changed Domain for vercel

--- src/services/auth.js
+++ src/services/auth.js
@@ -3,7 +3,7 @@ import auth0 from 'auth0-js';
 const auth = new auth0.WebAuth({
   domain: 'dev-r8h4horutpz3j3g6.us.auth0.com',
   clientID: 'zc9VbPxP1FnqLJ8pK5YiGwrF0w45x9rM',
-  redirectUri: 'http://localhost:5173/callback',
+  redirectUri: 'https://url-shortner-gold.vercel.app/callback',
   responseType: 'token id_token',
   scope: 'openid profile email',
 });
