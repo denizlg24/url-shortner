c52198bcb40c2924c608db4751e8a4e25d155e29
denizlg24 <denizlg24@gmail.com>
2023-03-01 10:27:37 +0000

    css adjustments and audience to try and prevent er

--- src/components/LinkItem.jsx
+++ src/components/LinkItem.jsx
@@ -6,7 +6,7 @@ const LinkItem = (props) =>{
         props.onClickHandler(props.shortUrl);
     }
     return (
-        <div className="link-container" style={{border: "2px solid " + (props.dark ? "rgba(255,255,255, 0.2)" : "rgba(0,0,0, 0.2)")}}>
+        <div className="link-container" style={{border: "2px dashed " + (props.dark ? "rgba(255,255,255, 0.2)" : "rgba(0,0,0, 0.2)")}}>
             <div className="link-info">
                 <h3 id='long-url-linkitem'>Long URL: <a href={props.longUrl} target="_blank">{(props.longUrl.length < 45) ? props.longUrl : props.longUrl.slice(0,45) + "..."}</a></h3>
                 <h3 id='long-url-linkitem'>Short URL: <a href={props.shortUrl} target="_blank">{props.shortUrl.slice(8)}</a></h3>
